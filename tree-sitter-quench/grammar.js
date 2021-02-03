7ae3e50fd1df7d8d5a0eb9e5b44d8a22aa47b715
Sam Estep <sam@samestep.com>
2021-02-02 21:43:26 -0800

    Add initial Tree-sitter parser

---
+++ tree-sitter-quench/grammar.js
@@ -0,0 +1,34 @@
+module.exports = grammar({
+  name: 'QUENCH',
+
+  extras: $ => [/\s/, $.comment],
+
+  rules: {
+    source_file: $ => repeat($._statement),
+
+    comment: $ => /#.*/,
+
+    _statement: $ => seq(choice($._expression), ';'),
+
+    _expression: $ => choice($._literal, $.identifier, $.call),
+
+    _literal: $ => choice($.string),
+
+    string: $ => /"[^"]*"/,
+
+    identifier: $ => /\w+/,
+
+    call: $ => seq(
+      field('function', $.identifier),
+      '(',
+      field('arguments', $.arguments),
+      ')',
+    ),
+
+    arguments: $ => seq(
+      $._expression,
+      repeat(seq(',', $._expression)),
+      optional(','),
+    ),
+  },
+});
