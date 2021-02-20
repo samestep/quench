a0ce042d9edb1aad200400b1467d4e9904c91730
Sam Estep <sam@samestep.com>
2021-02-19 17:46:14 -0800

    Stub out tests for VS Code extension

---
+++ editors/code/src/test/suite/extension.test.ts
@@ -0,0 +1,14 @@
+import * as assert from 'assert';
+import { after } from 'mocha';
+import * as vscode from 'vscode';
+
+suite('Extension Test Suite', () => {
+  after(() => {
+    vscode.window.showInformationMessage('All tests done!');
+  });
+
+  test('Sample test', () => {
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  });
+});
