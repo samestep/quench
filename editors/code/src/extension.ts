f7739d7e364af1a044b908d07a5660dad8f8a5b8
Sam Estep <sam@samestep.com>
2021-03-24 20:14:35 -0700

    Include version in quench-lsp path

--- editors/code/src/extension.ts
+++ editors/code/src/extension.ts
@@ -10,16 +10,16 @@ import * as lsp from 'vscode-languageclient/node';
 
 let client: lsp.LanguageClient;
 
-function defaultServerPath(): string {
-  return path.join(os.homedir(), '.quench', 'bin', 'quench-lsp');
+function defaultServerPath(version: string): string {
+  return path.join(os.homedir(), '.quench', version, 'bin', 'quench-lsp');
 }
 
-function serverPath(): string {
+function serverPath(version: string): string {
   const conf = vscode.workspace.getConfiguration('quench').get('server.path');
   if (typeof conf === 'string') {
     return conf;
   } else {
-    return defaultServerPath();
+    return defaultServerPath(version);
   }
 }
 
@@ -46,12 +46,12 @@ function start(command: string) {
 export async function activate(context: vscode.ExtensionContext) {
   const manifest = path.join(context.extensionPath, 'package.json');
   const { version } = JSON.parse(await fs.readFile(manifest, 'utf8'));
-  let server = serverPath();
+  let server = serverPath(version);
   const exists = await fs.stat(server).then(() => true, () => false);
   if (exists) {
     start(server);
   } else {
-    server = defaultServerPath();
+    server = defaultServerPath(version);
     const dir = path.dirname(server);
     // mkdir succeeds if dir already exists, since we set recursive to true
     await fs.mkdir(dir, { 'recursive': true });
