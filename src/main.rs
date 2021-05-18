d1382dcef0802f6930b7f2660492b798e476d46c
Sam Estep <sam@samestep.com>
2021-05-17 19:02:50 -0700

    Upgrade Deno to 1.10.2

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ mod syntax;
 mod text;
 
 use crate::{codegen::Codegen, db::QueryGroup, diagnosis::Diagnostic, opts::Opts};
+use deno_runtime::deno_file::BlobUrlStore;
 use std::{env, fs, path::PathBuf, rc::Rc, sync::Arc};
 use structopt::StructOpt;
 use url::Url;
@@ -97,7 +98,7 @@ fn compile(file: &PathBuf, opts: Opts) -> anyhow::Result<String> {
     Err(anyhow::anyhow!("Failed to compile."))
 }
 
-const DENO_VERSION: &str = "1.8.3";
+const DENO_VERSION: &str = "1.10.2";
 
 fn run(file: PathBuf, args: Vec<String>, opts: Opts) -> anyhow::Result<()> {
     let js = compile(&file, opts.clone())?;
@@ -109,7 +110,7 @@ fn run(file: PathBuf, args: Vec<String>, opts: Opts) -> anyhow::Result<()> {
         debug_flag: false,
         unstable: false,
         ca_data: None,
-        // https://github.com/denoland/deno/blob/v1.8.3/cli/version.rs
+        // https://github.com/denoland/deno/blob/v1.10.2/cli/version.rs
         user_agent: format!("Deno/{}", DENO_VERSION),
         seed: None,
         module_loader: Rc::new(loader::FixedLoader {
@@ -124,11 +125,13 @@ fn run(file: PathBuf, args: Vec<String>, opts: Opts) -> anyhow::Result<()> {
         should_break_on_first_statement: false,
         runtime_version: DENO_VERSION.to_string(),
         // Quench doesn't compile to TypeScript so this is just a placeholder that shouldn't matter
-        // https://github.com/denoland/deno/blob/v1.8.3/runtime/examples/hello_runtime.rs#L38
+        // https://github.com/denoland/deno/blob/v1.10.2/runtime/examples/hello_runtime.rs#L39
         ts_version: "x".to_string(),
         no_color: no_color::is_no_color(),
         get_error_class_fn: None,
         location: None,
+        location_data_dir: None,
+        blob_url_store: BlobUrlStore::default(),
     };
     let mut worker = deno_runtime::worker::MainWorker::from_options(
         main_module.clone(),
@@ -163,41 +166,5 @@ fn main() -> anyhow::Result<()> {
         // https://tokio.rs/tokio/tutorial/hello-tokio
         Sub::Lsp => Ok(tokio::runtime::Runtime::new()?.block_on(lsp::main(opts))),
         Sub::Run { file, args } => run(file, args, opts),
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use itertools::Itertools;
-    use regex::Regex;
-
-    #[test]
-    fn test_deno_version() {
-        let deno_core_version = fs::read_to_string("Cargo.toml")
-            .unwrap()
-            .parse::<toml::Value>()
-            .unwrap()
-            .get("dependencies")
-            .unwrap()
-            .get("deno_core")
-            .unwrap()
-            .as_str()
-            .unwrap()
-            .to_string();
-        let (_, minor, patch) = Regex::new(r"0.(\d)(\d)")
-            .unwrap()
-            .captures(&deno_core_version)
-            .unwrap()
-            .iter()
-            .collect_tuple()
-            .unwrap();
-        // I don't know if this correspondence is documented, but that doesn't really matter here
-        // anyway since the point here is just to avoid accidentally updating the deno_core version
-        // in Cargo.toml without also updating DENO_VERSION here
-        assert_eq!(
-            DENO_VERSION,
-            format!("1.{}.{}", minor.unwrap().as_str(), patch.unwrap().as_str())
-        );
     }
 }
