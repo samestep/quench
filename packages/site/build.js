b12a718b1b5ec9e88abb34c15b32a529d6504dc2
Sam Estep <sam@samestep.com>
2022-06-11 18:30:26 -0400

    Don't use .. to find Wasm files

--- packages/site/build.js
+++ packages/site/build.js
@@ -1,10 +1,12 @@
 import * as fs from "fs/promises";
+import { createRequire } from "module";
 import * as path from "path";
 
-const dir = "public";
-await fs.mkdir(dir, { recursive: true }); // don't throw if it already exists
+const require = createRequire(import.meta.url);
+const from = path.dirname(require.resolve("web-tree-sitter"));
+
+const to = "public";
+await fs.mkdir(to, { recursive: true }); // don't throw if it already exists
+
 const filename = "tree-sitter.wasm";
-await fs.copyFile(
-  path.join("../../node_modules/web-tree-sitter", filename),
-  path.join(dir, filename)
-);
+await fs.copyFile(path.join(from, filename), path.join(to, filename));
--- packages/site/script.js
+++ packages/site/script.js
@@ -1,11 +1,11 @@
 import { main } from "@quench-lang/core";
-import parserWasmUrl from "../tree-sitter/tree-sitter-quench.wasm?url";
+import parseWasmUrl from "@quench-lang/tree-sitter/tree-sitter-quench.wasm?url";
 import Parser from "web-tree-sitter";
 
 (async () => {
   await Parser.init();
   const parser = new Parser();
-  const Quench = await Parser.Language.load(parserWasmUrl);
+  const Quench = await Parser.Language.load(parseWasmUrl);
   parser.setLanguage(Quench);
   document.getElementById("p").innerText = await main(parser);
 })();
